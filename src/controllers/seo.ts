ee3440028447c79280461ffb1e6d38d3ae491e19
Nikolay Dorofeev <d.orich@yandex.com>
2021-08-08 20:32:55 +0700

    New sitemap algorithm

--- src/controllers/seo.ts
+++ src/controllers/seo.ts
@@ -1,12 +1,12 @@
 import {FastifyReply, FastifyRequest} from "fastify"
 import {boomify} from 'boom'
-import {authDb, projectsDb, resumeDb} from '../server'
+import {authDb, newsDb, projectsDb, resumeDb} from '../server'
 import xmlbuilder from "xmlbuilder";
 
 export const generateSiteMap = () => {
     return async (req: FastifyRequest, rep: FastifyReply) => {
         try {
-            const baseUrl = 'https://d0rich.github.io/#'
+            const baseUrl = 'https://d0rich.github.io'
             const sitemap = xmlbuilder.create('urlset', {encoding: 'UTF-8', version: '1.0'})
                 .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
                 .att('xmlns:xhtml', 'http://www.w3.org/1999/xhtml')
@@ -54,6 +54,7 @@ export const generateSiteMap = () => {
             // Создание xml
             addUrl(sitemap, '/', 1.0, undefined, 'monthly')
             addUrl(sitemap, '/portfolio', 0.8, undefined, 'monthly')
+            addUrl(sitemap, '/news', 0.5, undefined, 'monthly')
             const projects = await projectsDb.projects.findAll({
                 attributes: ['stringId', 'updatedAt'],
                 order: [['date', 'DESC']]
@@ -61,12 +62,17 @@ export const generateSiteMap = () => {
             projects.forEach(project => {
                 addUrl(sitemap, `/portfolio/${project.stringId}`, 0.7 , project.updatedAt)
             })
-            // addUrl(sitemap, '/about', 0.95, undefined, 'monthly')
             const publicResume = await resumeDb.resume
                 .where('show', "==", true)
                 .get()
             publicResume.docs.forEach(resume => {
-                addUrl(sitemap, `/about/resume/${resume.id}`, 0.3, undefined, 'monthly')
+                addUrl(sitemap, `/resume/${resume.id}`, 0.5, undefined, 'monthly')
+            })
+            const news = await newsDb.news.findAll()
+            news.forEach(n => {
+                const date = n.createdAt
+                addUrl(sitemap, `/news/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`+
+                    `/${n.stringId}`, 0.3, undefined)
             })
             rep.type('text/xml')
             rep.header('charset', 'UTF-8')
